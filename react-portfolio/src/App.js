07849837aabf9f0a52eafe1791c98e935d959a3b
avenje <jeremyavenarius@gmail.com>
2018-05-12 17:56:02 -0600

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Welcome from './components/Welcome';
+import Projects from './components/Projects';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <NavBar />
+          <Welcome />
+        <h2 className="heading" id="projects">PROJECTS</h2>
+          <Projects />
+        <h2 className="heading" id="about">ABOUT</h2>
+          <About />
+        <h2 className="heading" id="footer">LIKE WHAT YOU SEE?</h2>
+          <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
