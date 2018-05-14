b4c6db2828621cfc666d8d5beeb52be6d37d66b6
avenje <jeremyavenarius@gmail.com>
2018-05-13 18:56:01 -0600

    added pic

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,10 +13,10 @@ class Projects extends Component {
                <div className="overlay">
                   <div className="text">
                      <h3>The Barista</h3>
-                     <p>Search for the nearest coffee shop</p>
+                     <p><em>Search for the nearest coffee shop and <br/> find out their tip ratio</em></p>
                      <p>JavaScript, JQuery, AJAX, APIs, CSS, SASS</p>
-                     <p><a href="https://github.com/aaronwey/project1" target="_blank" rel="noopener noreferrer">Repository(Group Project)<i className="fa fa-github" aria-hidden="true"/></a></p>
-                     <p><a href="https://ccrs2006.github.io/barista/" target="_blank" rel="noopener noreferrer">Live Demo Site<i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://github.com/aaronwey/project1" target="_blank" rel="noopener noreferrer">Repository(Group Project) <i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://ccrs2006.github.io/barista/" target="_blank" rel="noopener noreferrer">Live Demo Site <i className="fa fa-github" aria-hidden="true"/></a></p>
                   </div>
                </div> 
             </div>
@@ -25,10 +25,10 @@ class Projects extends Component {
                <div className="overlay">
                   <div className="text">
                      <h3>Train Scheduler</h3>
-                     <p>Fictional Train Schedule for Arriving Trains and Newly Created Trains</p>
+                     <p><em>Fictional Train Schedule for Arriving Trains and <br/> Newly Created Trains</em></p>
                      <p>Firebase, Moment.js, JavaScript, JQuery, CSS, Bootstrap</p>
-                     <p><a href="https://github.com/avenje/train_scheduler" target="_blank" rel="noopener noreferrer">Repository<i className="fa fa-github" aria-hidden="true"/></a></p>
-                     <p><a href="https://avenje.github.io/train_scheduler/" target="_blank" rel="noopener noreferrer">Live Demo Site<i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://github.com/avenje/train_scheduler" target="_blank" rel="noopener noreferrer">Repository <i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://avenje.github.io/train_scheduler/" target="_blank" rel="noopener noreferrer">Live Demo Site <i className="fa fa-github" aria-hidden="true"/></a></p>
                   </div>
                </div> 
             </div>
@@ -37,10 +37,10 @@ class Projects extends Component {
                <div className="overlay">
                   <div className="text">
                      <h3>Scraped Android News</h3>
-                     <p>Android News Scraped from a News Site</p>
+                     <p><em>Android News Scraped from a News Site</em></p>
                      <p>Node.js, Express, MongoDB with Mongoose, JavaScript,<br/> JQuery, Handlebars, CSS, Bootstrap</p>
-                     <p><a href="https://github.com/avenje/scrape-news" target="_blank" rel="noopener noreferrer">Repository<i className="fa fa-github" aria-hidden="true"/></a></p>
-                     <p><a href="https://tranquil-dawn-96654.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo Site<i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://github.com/avenje/scrape-news" target="_blank" rel="noopener noreferrer">Repository <i className="fa fa-github" aria-hidden="true"/></a></p>
+                     <p><a href="https://tranquil-dawn-96654.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo Site <i className="fa fa-github" aria-hidden="true"/></a></p>
                   </div>
                </div> 
             </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import bw_1 from '../images/bw_1.jpg';
 // import {accessId, APIkey} from '../secrets.js'
 
 class Welcome extends Component {
    render() {
       return (
          <div className="Welcome section">
-            <h1 className="welcome-intro"><strong>Jeremy Avenarius</strong> is seeking out an opportunity to be a Web Developer in Denver, CO.</h1>
-            <br/>
-            <br/>
-            <a href="https://www.linkedin.com/in/jeremy-avenarius/" target="_blank" alt="LinkedIn link" rel="noopener noreferrer"><i className="fa fa-linkedin-square i-hov" aria-hidden="true"/></a>
-            <a href="https://github.com/avenje" target="_blank" alt="Github link" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true"/></a>
-            <a href="mailto:jeremyavenarius@gmail.com?Subject=Sent%20from%20Jeremy's%20portfolio%20page" target="_top" alt="GMail link"><i className="fa fa-envelope i-hov" aria-hidden="true"/></a>
-            <br/>
-            <a href="https://drive.google.com/open?id=1eau5_1d_IXBq3sTSAk-RRbkmB1DDFm1F" target="_blank" alt="Resume link" rel="noopener noreferrer"><button className="button resume-button">Resume <i className="fa fa-file-pdf-o" aria-hidden="true"/></button></a>
-
+            <h1 className="welcome-intro"><strong>Jeremy Avenarius</strong> is seeking out an opportunity to be a Web Developer in Denver, CO.
+               <br/>
+               <br/>
+               <a href="https://www.linkedin.com/in/jeremy-avenarius/" target="_blank" alt="LinkedIn link" rel="noopener noreferrer"><i className="fa fa-linkedin-square i-hov" aria-hidden="true"/></a>
+               <a href="https://github.com/avenje" target="_blank" alt="Github link" rel="noopener noreferrer"><i className="fa fa-github i-hov" aria-hidden="true"/></a>
+               <a href="mailto:jeremyavenarius@gmail.com?Subject=Sent%20from%20Jeremy's%20portfolio%20page" target="_top" alt="GMail link"><i className="fa fa-envelope i-hov" aria-hidden="true"/></a>
+               <br/>
+               <a href="https://drive.google.com/open?id=1eau5_1d_IXBq3sTSAk-RRbkmB1DDFm1F" target="_blank" alt="Resume link" rel="noopener noreferrer"><button className="button resume-button">Resume <i className="fa fa-file-pdf-o" aria-hidden="true"/></button></a>
+            </h1>
+            <img src={bw_1} className="main-pic"/>
          </div>
       );
    }
