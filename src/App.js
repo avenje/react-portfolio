d85720e83c88bb78f52caaadec0f1e3638045a1a
avenje <jeremyavenarius@gmail.com>
2018-05-19 15:05:20 -0600

    updates to About

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ class App extends Component {
           <NavBar />
           <Welcome />
         <h2 className="heading" id="projects">PROJECTS</h2>
+        <h4>More to come...</h4>
           <Projects />
-        <h2 className="heading" id="about">ABOUT</h2>
+        <h2 className="heading" id="about">ABOUT ME</h2>
           <About />
-        <h2 className="heading" id="footer">Contact Info</h2>
+        <h2 className="heading" id="footer">CONTACT INFO</h2>
           <Footer />
       </div>
     );
